ec6f3136593cf761d2583bc69faaf8149f1d475d
KanavRampal <kanav94@gmail.com>
2020-08-17 05:47:40 +0530

    First commit

---
+++ Controllers/OrderController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace POC.Order.API.Controllers
+{
+    [Route("api/orders")]
+    [ApiController]
+    public class OrderController : ControllerBase
+    {
+        [HttpGet, Route("")]
+        public IEnumerable<Order> Get()
+        {
+            List<Order> retVal = new List<Order>();
+            retVal.Add(new Order(1, 250, DateTime.Parse("14-Apr-2020").Date));
+            retVal.Add(new Order(2, 450, DateTime.Parse("15-Apr-2020").Date));
+            return retVal;
+            //List<Order> retVal =  GetOrderList();
+            //return retVal;
+        }
+
+        private List<Order> GetOrderList()
+        {
+            List<Order> retVal = new List<Order>();
+            retVal.Add(new Order(1, 250, DateTime.Parse("14-Apr-2020")));
+            retVal.Add(new Order(2, 450, DateTime.Parse("15-Apr-2020")));
+            return  retVal;
+        }
+    }
+}
---
+++ Model/Order.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace POC.Order.API
+{
+    public class Order
+    {
+        public int OrderId { get; set; }
+        public decimal OrderTotal { get; set; }
+        public DateTime OrderDate { get; set; }
+
+        public Order(int id, decimal orderTotal, DateTime date)
+        {
+            this.OrderId = id;
+            this.OrderTotal = orderTotal;
+            this.OrderDate = date;
+        }
+    }
+}
